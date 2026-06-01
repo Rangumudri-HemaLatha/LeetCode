c8a27e1440a9b8ef5427473bc3f0e10b4c0ad79f
Rangumudri Hema Latha <Rangumudrihemanaidu@gmail.com>
2026-06-01 21:14:45 +0530

    Rename  for clarity in reverse method

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int reverse(int x) {
-        int res = 0;
+        int result = 0;
         while (x != 0) {
             int ld = x % 10;
-            if (res > INT_MAX / 10 || (res == INT_MAX / 10 && ld > 7))
+            if (result > INT_MAX / 10 || (result == INT_MAX / 10 && ld > 7))
                 return 0;
-            if (res < INT_MIN / 10 || (res == INT_MIN / 10 && ld < -8))
+            if (result < INT_MIN / 10 || (result == INT_MIN / 10 && ld < -8))
                 return 0;
-            res = res * 10 + ld;
+            result = result * 10 + ld;
             x /= 10;
         }
-        return res;
+        return result;
     }
 };
